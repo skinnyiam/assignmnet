46d197f7fb6dd6e4eb98ea935122aecaf2cd7ea6
Saurabh Shukla <ss6408367@gmail.com>
2024-11-22 21:38:10 +0530

    fixed the frninednd

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/v1", insightRoutes);
-
+//FWFWEF
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
